a96f931288653cbb6db39e490ef464bf902eec41
[Nabila] <[nazrulislamdobon@gmail.com]>
2018-03-07 21:32:21 -0500

    I have done some correction

--- src/test/java/step_definitions/GoogleFooterLinkTests.java
+++ src/test/java/step_definitions/GoogleFooterLinkTests.java
@@ -1,59 +1,60 @@
 package step_definitions;
-
+import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import helper_classes.SetDriver;
 
-public class GoogleFooterLinkTests {
+public class GoogleFooterLinkTests{
 
+	private static WebDriver driver;
 
-	public WebDriver driver;
 	public GoogleFooterLinkTests() {
-		driver= SetDriver.webDriver;
+		driver = SetDriver.webDriver;
+//		System.out.println("This is the Nazrul07....");
 	}
 
 
-
-	@Given("^I launch the Google home page url$")
-	public void i_launch_the_Google_home_page_url() throws Throwable {
-
-		driver.get("https://www.google.com");
-
+	@When("^I click on the Privacy link$")
+	public void i_click_on_the_Privacy_link() throws Throwable {
+		driver.findElement(By.linkText("Privacy")).click();
+		Thread.sleep(2000);   // temporary wait
 	}
 
-
-	@Given("^I scroll down to the end of the page$")
-	public void i_scroll_down_to_the_end_of_the_page() throws Throwable {
-
-		((JavascriptExecutor) driver).executeScript("window.scrollTo(0,document.body.scrollHeight)");
-		Thread.sleep(2000);
+	@Then("^I see the Privacy page$")
+	public void i_see_the_Privacy_page() throws Throwable {
+		Assert.assertEquals(true, driver.getPageSource().contains("Welcome to the Google Privacy Policy"));
+		Thread.sleep(2000);   // temporary wait
 	}
 
+	@When("^I click on the Terms link$")
+	public void i_click_on_the_Terms_link() throws Throwable {
+		driver.findElement(By.linkText("Terms")).click();
+		Thread.sleep(2000);   // temporary wait
+	}
 
-	@When("^I select \"([^\"]*)\" link in the Google footer$")
+	@Then("^I see the Terms page$")
+	public void i_see_the_Terms_page() throws Throwable {
+		Assert.assertEquals(true, driver.getPageSource().contains("Google Terms of Service"));
+		Thread.sleep(2000);   // temporary wait
+	}
+	
+	@When("^I select \"(.*?)\" link in the Google footer$")
 	public void i_select_link_in_the_Google_footer(String footerLink_text) throws Throwable {
-
-
-		By linkedElement = By.xpath("//a[contains(text(),'" + footerLink_text + "')]");
-		driver.findElement(linkedElement).click();
-		Thread.sleep(2000);
-
+		
+		driver.findElement(By.linkText(footerLink_text)).click();
+		
+		Thread.sleep(2000);   // temporary wait
 	}
 
-
-	@Then("^I verified the selected Google page by the existence of the \"([^\"]*)\" text$")
-	public void i_verified_the_selected_Google_page_by_the_existence_of_the_tlinkedPage_textext(String linkedPage_text) throws Throwable {
-
+	@Then("^I verified the selected Google page by the existence of the \"(.*?)\" text$")
+	public void i_verified_the_selected_Google_page_by_the_existence_of_the_text(String linkedPage_text) throws Throwable {
 		Assert.assertEquals(true, driver.getPageSource().contains(linkedPage_text));
-		Thread.sleep(2000);
+		Thread.sleep(2000);   // temporary wait
 	}
-
 
 	@Given("^I select the Privacy link in the Google footer$")
 	public void i_select_the_Privacy_link_in_the_Google_footer() throws Throwable {
@@ -111,4 +112,5 @@ public class GoogleFooterLinkTests {
 		Assert.assertEquals(true, driver.getPageSource().contains(Technology_detail));
 		Thread.sleep(2000);
 	}
+	
 }
